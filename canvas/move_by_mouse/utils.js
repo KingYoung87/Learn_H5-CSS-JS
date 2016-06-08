25004db3c5a46b372afbe8f1b841595b0c42b0cc
岳洋 <Young@YoungdeMacBook-Pro-2.local>
2016-06-08 16:06:01 +0800

    add files

---
+++ canvas/move_by_mouse/utils.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Young on 16/6/3.
+ */
+function utils(){};
+utils.captureMouse=function(element){
+    var mouse={x:0,y:0};
+
+    element.addEventListener('mousemove',function(event){
+        var x,y;
+        if(event.pageX || event.pageY){
+            x=event.pageX;
+            y=event.pageY;
+        }else{
+            x=event.clientX+document.body.scrollLeft+
+                document.documentElement.scrollLeft;
+            y=event.clientY+document.body.scrollTop+
+                document.documentElement.scrollTop;
+        }
+        x -= element.offsetLeft;
+        y -= element.offsetTop;
+
+        mouse.x=x;
+        mouse.y=y;
+    },false);
+
+    return mouse;
+};
